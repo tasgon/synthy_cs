11fbdc87c4c8f7157f6be7111e3dab4d96371263
telastrus <10052313+telastrus@users.noreply.github.com>
2020-11-13 23:51:22 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Melanchall.DryWetMidi.Devices;
+
+namespace synthy_cs
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("printing devices");
+            foreach (var dev in InputDevice.GetAll())
+            {
+                Console.WriteLine(dev.Name);
+            }
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
